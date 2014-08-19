15acfe733b7df9348b09a9faf8bc37e30848a44a
Martin Richter <lawn.mower@freenet.de>
2014-08-19 22:34:21 +0200

    add bootstrap code for Lu, Alex and questions
    Thereby removing it from the test.

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,7 +1,34 @@
+import hightime.QuestionSubject
+import hightime.Question
+
 class BootStrap {
 
-    def init = { servletContext ->
-    }
-    def destroy = {
-    }
+  def init = { servletContext ->
+    def lu = new QuestionSubject(name: "Lu")
+    def alex = new QuestionSubject(name: "Alex")
+
+    lu.addToQuestions(
+      new Question(question: "Was ist Ihr Lieblingsgetränk?",
+                   subject: lu))
+
+    lu.addToQuestions(
+      new Question(question: """\
+Wenn sie ihm einen Pullover kaufte, welche Farbe würde sie wählen?""",
+                   subject: lu))
+
+    alex.addToQuestions(
+      new Question(question: """\
+In welches Land würde er gern als nächstes reisen?""",
+                   subject: alex))
+
+    alex.addToQuestions(
+      new Question(question: """\
+Welches Gewürz benutzt er am liebsten beim Kochen?""",
+                   subject: alex))
+
+    lu.save(failOnError: true)
+    alex.save(failOnError: true)
+  }
+  def destroy = {
+  }
 }
--- test/unit/hightime/QuestionSpec.groovy
+++ test/unit/hightime/QuestionSpec.groovy
@@ -16,27 +16,5 @@ class QuestionSpec extends Specification {
     }
 
     void "test something"() {
-      def lu = new QuestionSubject(name: "Lu")
-      def alex = new QuestionSubject(name: "Alex")
-      def q
-
-      q = new Question(question: "Was ist Ihr Lieblingsgetränk?")
-      q.subject = lu
-      lu.questions << q
-
-      q = new Question(question: """\
-Wenn sie ihm einen Pullover kaufte, welche Farbe würde sie wählen?""")
-      q.subject = lu
-      lu.questions << q
-
-      q = new Question(question: "In welches Land würde er gern als nächstes reisen?")
-      q.subject = alex
-      alex.questions << q
-
-      q = new Question(question: """\
-Welches Gewürz benutzt er am liebsten beim Kochen?""")
-      q.subject = alex
-      alex.questions << q
-
     }
 }
